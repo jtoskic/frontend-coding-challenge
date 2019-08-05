3b60b5ae62115646230fd26224b5c6d4d0a190e7
Jovan Toskic <jtoskic@itekako.com>
2019-08-05 12:09:34 +0200

    Added loader, tabs funcionality and style

--- src/App.js
+++ src/App.js
@@ -11,56 +11,63 @@ class App extends Component {
   state = {
       offices:[],
       loading:true,
+      index:0
   }
 
   render() {
     return (
         <div className='app'>
-            <Tabs>
-                <header className='header'>
-                    <div className="headline">
-                        <h2>Offices</h2>
-                    </div>
+            { this.state.loading ? <img
+                    className='loading'
+                    src="https://media.giphy.com/media/sSgvbe1m3n93G/giphy.gif"
+                    alt="loading"/>
+            :
+                <Tabs onSelect={index => this.isActive(index)}>
+                    <header className='header'>
+                        <div className="headline">
+                            <h2>Offices</h2>
+                        </div>
 
-                    <TabList className='navigation'>
-                        <Tab>List</Tab>
-                        <Tab>Grid</Tab>
-                        <Tab>Map</Tab>
-                    </TabList>
-                </header>
+                        <TabList className='navigation'>
+                            <Tab style={this.state.index === 0 ? styles.active : styles.notActive}>List</Tab>
+                            <Tab style={this.state.index === 1 ? styles.active : styles.notActive}>Grid</Tab>
+                            <Tab style={this.state.index === 2 ? styles.active : styles.notActive}>Map</Tab>
+                        </TabList>
+                    </header>
 
-                <TabPanel className='list-container'>
-                    {this.state.offices.map((office) => {
-                        return <List
-                            key={office.id}
-                            name={office.name}
-                            description={office.description}
-                            photo={office.photo}
-                        />
-                    })}
-                </TabPanel>
+                    <TabPanel className='list-container'>
+                        {this.state.offices.map((office) => {
+                            return <List
+                                key={office.id}
+                                name={office.name}
+                                description={office.description}
+                                photo={office.photo}
+                            />
+                        })}
+                    </TabPanel>
 
-                <TabPanel className='grid-container'>
-                    {this.state.offices.map((office) => {
-                        return <Grid
-                            key={office.id}
-                            name={office.name}
-                            description={office.description}
-                            photo={office.photo}
-                        />
-                    })}
-                </TabPanel>
+                    <TabPanel className='grid-container'>
+                        {this.state.offices.map((office) => {
+                            return <Grid
+                                key={office.id}
+                                name={office.name}
+                                description={office.description}
+                                photo={office.photo}
+                            />
+                        })}
+                    </TabPanel>
 
-                <TabPanel>
-                    {this.state.offices.map((office) => {
-                        return <OfficeMap
-                            key={office.id}
-                            lat={office.latitude}
-                            lng={office.longitude}
-                        />
-                    })}
-                </TabPanel>
-            </Tabs>
+                    <TabPanel>
+                        {this.state.offices.map((office) => {
+                            return <OfficeMap
+                                key={office.id}
+                                lat={office.latitude}
+                                lng={office.longitude}
+                            />
+                        })}
+                    </TabPanel>
+                </Tabs>
+            }
         </div>
     )
   }
@@ -74,13 +81,28 @@ class App extends Component {
           .then((response) => {
               if(response.status === 200 || response.status === 201) {
                   this.setState({offices:response.data})
+                  this.setState({loading:false})
               }
           })
           .catch((error) => {
               console.log(error)
           })
   }
+
+  isActive = (index) => {
+      this.setState({index:index})
+  }
 }
 
+const styles = {
+    active: {
+        borderBottom:'1.5px solid #ffff4d',
+        color:'#fff'
+    },
+    notActive: {
+        borderBottom:'1.5px solid transparent',
+        color:'#a6dce8'
+    }
+}
 
 export default App;
